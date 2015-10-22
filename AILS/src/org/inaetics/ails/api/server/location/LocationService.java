dd4f8cfdb23a91f21d0ad7eacbc81dd6edf2f67c
Nico Korthout <nicokorthout@gmail.com>
2015-10-22 13:17:13 +0200

    Minor Javadoc change for LocationService

--- AILS/src/org/inaetics/ails/api/server/location/LocationService.java
+++ AILS/src/org/inaetics/ails/api/server/location/LocationService.java
@@ -10,7 +10,7 @@ import org.inaetics.ails.api.model.User;
  * This service provides functions to find {@link Location Locations} of {@link User Users}.
  * 
  * @author nicokorthout
- * @version 0.1.0
+ * @version 0.1.1
  * @since 22-10-2015
  */
 public interface LocationService {
@@ -18,9 +18,10 @@ public interface LocationService {
     /**
      * Find the Location of a {@link User}.
      * 
-     * @param uuid @NotNull Universally unique identifier identifying the User to get the Location from. 
+     * @param uuid @NotNull Universally unique identifier identifying the User of which to find the
+     *        Location.
      * @return An Optional containing the Location object if found, or an empty Optional if not.
      */
     Optional<Location> findByUserUUID(UUID uuid);
-    
+
 }
