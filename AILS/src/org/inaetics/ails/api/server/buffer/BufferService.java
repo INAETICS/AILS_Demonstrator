61c8c5c1318d46cd8406a68e5cd499aecef98584
Nico Korthout <nicokorthout@gmail.com>
2015-10-28 16:36:34 +0100

    Added size method to BufferService

--- AILS/src/org/inaetics/ails/api/server/buffer/BufferService.java
+++ AILS/src/org/inaetics/ails/api/server/buffer/BufferService.java
@@ -1,28 +1,35 @@
 package org.inaetics.ails.api.server.buffer;
 
-
 /**
- * This service provides a buffer.
+ * A buffer can be used to temporarily store objects before processing. The buffer provides ways to
+ * add and remove elements and to check its size.
  * 
  * @author L. Buit, N. Korthout, J. Naus
- * @version 0.1.0
+ * @version 1.0.0
  * @since 28-10-2015
+ * @param <E> The type of elements this buffer can handle.
  */
 public interface BufferService<E> {
 
     /**
-     * Add an element to the buffer. 
+     * Add an element to the buffer.
      * 
      * @param e @NotNull the type of elements held in this collection.
-     * 
-     * @return true if element was added successfully.
      */
-    boolean add(E e);
-    
+    void add(E e);
+
     /**
      * Removes an element from the buffer.
      * 
      * @return the element to remove.
      */
     E remove();
+
+    /**
+     * Returns the number of elements in this buffer.
+     * 
+     * @return the number of elements in this buffer.
+     */
+    int size();
+
 }
