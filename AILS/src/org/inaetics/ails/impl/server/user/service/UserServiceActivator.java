c92f178770e0d1bc4dc876cde1200ad9798e0313
Nico Korthout <nicokorthout@gmail.com>
2015-10-29 15:53:27 +0100

    Added a run descriptor for the project and an activator for the UserService

---
+++ AILS/src/org/inaetics/ails/impl/server/user/service/UserServiceActivator.java
@@ -0,0 +1,20 @@
+package org.inaetics.ails.impl.server.user.service;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class UserServiceActivator implements BundleActivator {
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        // TODO Auto-generated method stub
+        System.out.println("Yay imma running ^^,");
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        // TODO Auto-generated method stub
+        System.out.println("aaaah i stop for youuuu --");
+    }
+
+}
---
+++ AILS/src/org/inaetics/ails/impl/server/user/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package org.inaetics.ails.impl.server.user.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.inaetics.ails.api.common.model.Location;
+import org.inaetics.ails.api.common.model.User;
+import org.inaetics.ails.api.server.user.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public void add(User user) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<User> find(UUID uuid) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Location> locate(UUID uuid) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
