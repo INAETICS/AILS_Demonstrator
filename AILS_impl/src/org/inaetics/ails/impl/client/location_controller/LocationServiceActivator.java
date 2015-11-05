10fb50f1728cff7951ffb9531380725662117d87
Naus Jessy <jessy.naus@etu.emse.fr>
2015-11-05 14:47:14 +0100

    Create the client Location Service Activator and Implementation. Added bundle descriptor.

---
+++ AILS_impl/src/org/inaetics/ails/impl/client/location_controller/LocationServiceActivator.java
@@ -0,0 +1,25 @@
+package org.inaetics.ails.impl.client.location_controller;
+
+import org.osgi.framework.BundleContext;
+
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+
+import org.inaetics.ails.api.client.location_controller.LocationService;
+
+/**
+ * The LocationServiceActivator starts the {@link LocationService LocationService}.
+ * 
+ * @author L. Buit, N. Korthout, J. Naus
+ * @version 0.1.0
+ * @since 05-11-2015
+ */
+public class LocationServiceActivator extends DependencyActivatorBase {
+
+    @Override
+    public void init(BundleContext context, DependencyManager dm) throws Exception {
+        dm.add(createComponent().setInterface(LocationService.class.getName(), null)
+                .setImplementation(LocationServiceImpl.class));
+    }
+
+}
---
+++ AILS_impl/src/org/inaetics/ails/impl/client/location_controller/LocationServiceImpl.java
@@ -0,0 +1,44 @@
+package org.inaetics.ails.impl.client.location_controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.inaetics.ails.api.client.location_controller.LocationService;
+import org.inaetics.ails.api.common.model.Location;
+import org.inaetics.ails.api.common.model.User;
+
+/**
+ * The LocationServiceImpl class provides an implementation of the {@link LocationService
+ * LocationService}
+ * 
+ * @author L. Buit, N. Korthout, J. Naus
+ * @version 0.1.0
+ * @since 05-11-2015
+ */
+public class LocationServiceImpl implements LocationService {
+
+    @Override
+    public List<User> getUsers() {
+        throw new UnsupportedOperationException(
+                "LocationServiceImpl.getUsers() not yet implemented.");
+    }
+
+    @Override
+    public void displayUserlist(List<User> users) {
+        throw new UnsupportedOperationException(
+                "LocationServiceImpl.displayUserlist(List<User> user) not yet implemented.");
+    }
+
+    @Override
+    public Optional<Location> queryUserLocation(User user) {
+        throw new UnsupportedOperationException(
+                "LocationServiceImpl.queryUserLocation(User user) not yet implemented.");
+    }
+
+    @Override
+    public void displayLocation(Location location) {
+        throw new UnsupportedOperationException(
+                "LocationServiceImpl.displayLocation(Location location) not yet implemented.");
+    }
+
+}
