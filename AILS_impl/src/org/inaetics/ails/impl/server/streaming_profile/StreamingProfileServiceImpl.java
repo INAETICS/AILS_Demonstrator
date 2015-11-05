4a6cc40b45be78f7090afbe0636c3dff80db7d74
Naus Jessy <jessy.naus@etu.emse.fr>
2015-11-05 11:06:31 +0100

    Deleted ExampleComponent. Deleted construct in StreamingProfileServiceImpl.

--- AILS_impl/src/org/inaetics/ails/impl/server/streaming_profile/StreamingProfileServiceImpl.java
+++ AILS_impl/src/org/inaetics/ails/impl/server/streaming_profile/StreamingProfileServiceImpl.java
@@ -4,8 +4,6 @@ import org.inaetics.ails.api.common.model.UserWiFiProfile;
 import org.inaetics.ails.api.server.streaming_profile.StreamingProfileService;
 import org.inaetics.ails.impl.server.buffer.BufferServiceImpl;
 
-import com.google.common.base.Preconditions;
-
 /**
  * The StreamingProfileServiceImpl class provides an implementation of the
  * {@link StreamingProvileService StreamingProfileService}
@@ -18,16 +16,7 @@ public class StreamingProfileServiceImpl implements StreamingProfileService {
 
     // Injected by Dependency Manager
     public volatile BufferServiceImpl buffer;
-
-    /**
-     * This constructs a Streaming Profile Service with a buffer.
-     * 
-     * @param buffer @NotNull The steaming profile buffer.
-     */
-    public StreamingProfileServiceImpl(BufferServiceImpl buffer) {
-        this.buffer = Preconditions.checkNotNull(buffer, "Buffer is not set");
-    }
-
+    
     @Override
     public void add(UserWiFiProfile userWiFiProfile) {
         buffer.add(userWiFiProfile);
