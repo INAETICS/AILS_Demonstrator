cdabc929416a2258245936586387ecfbfeab9824
Lennart Buit <lennartbuit@gmail.com>
2015-11-12 16:46:12 +0100

    Started implementing the Intergration Test
    For this bndtools has a provision, a project dedicated for testing so I generated
     this. For more information please read:
        - http://blog.knowhowlab.org/2013/03/osgi-integration-tests-with-bndtools.html

---
+++ AILS_test/src/org/inaetics/ails/intergration/UserIntergrationTest.java
@@ -0,0 +1,15 @@
+package org.inaetics.ails.intergration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserIntergrationTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+
+}
