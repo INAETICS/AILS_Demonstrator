149b9cb3bc427c113da08ae8ea732ca8c8b118b4
Naus Jessy <jessy.naus@etu.emse.fr>
2015-11-24 15:48:38 +0100

    Added the view interface with the corresponding .bnd file.

---
+++ AILS/src/org/inaetics/ails/api/client/view/View.java
@@ -0,0 +1,21 @@
+package org.inaetics.ails.api.client.view;
+
+import org.inaetics.ails.api.common.model.Location;
+import org.inaetics.ails.api.common.model.User;
+
+/**
+ * The View provides a way to ask the {@link User} for his/her current location.
+ * 
+ * @author L. Buit, N. Korthout, J. Naus
+ * @version 0.1.0
+ * @since 24-11-2015
+ */
+public interface View {
+
+    /**
+     * Ask the user for his/her current location.
+     * 
+     * @return The user's current location.
+     */
+    Location askUserForCurrentLocation();
+}
