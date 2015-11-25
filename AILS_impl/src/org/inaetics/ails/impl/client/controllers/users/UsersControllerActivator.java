fead38375f5abb3903d43b6a013bc04d62385cdf
Naus Jessy <jessy.naus@etu.emse.fr>
2015-11-25 16:39:13 +0100

    Implemented the UsersController and added an activator.

--- AILS_impl/src/org/inaetics/ails/impl/client/controllers/users/UsersControllerActivator.java
+++ AILS_impl/src/org/inaetics/ails/impl/client/controllers/users/UsersControllerActivator.java
@@ -3,14 +3,22 @@ package org.inaetics.ails.impl.client.controllers.users;
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
 import org.inaetics.ails.api.client.controllers.users.UsersController;
+import org.inaetics.ails.api.server.user.service.UserService;
 import org.osgi.framework.BundleContext;
 
+/**
+ * The UsersControllerActivator starts the {@link UsersController UsersController}.
+ * 
+ * @author L. Buit, N. Korthout, J. Naus
+ * @version 0.1.0
+ * @since 25-11-2015
+ */
 public class UsersControllerActivator extends DependencyActivatorBase {
 
     @Override
     public void init(BundleContext context, DependencyManager manager) throws Exception {
-        manager.add(createComponent().setInterface(UsersController.class.getName(), null));
-        
+        manager.add(createComponent().setInterface(UsersController.class.getName(), null)
+                .add(createServiceDependency().setService(UserService.class).setRequired(true)));
     }
 
 }
